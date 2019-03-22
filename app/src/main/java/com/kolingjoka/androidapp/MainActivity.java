511273a19d814a6ff1bb355a50b7bed528be2956
arditsarja <Brainstormsolutionsal@gmail.com>
2019-03-22 12:23:20 +0100

    button example

--- app/src/main/java/com/kolingjoka/androidapp/MainActivity.java
+++ app/src/main/java/com/kolingjoka/androidapp/MainActivity.java
@@ -1,15 +1,26 @@
 package com.kolingjoka.androidapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnClick);
+        button.setOnClickListener(MainActivity.this);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+    }
 }
